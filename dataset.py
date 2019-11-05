babfb37345ac8217ea84492c136482059e648347
Douzon <thibault.douzon@esker.corp>
2019-11-05 16:11:54 +0100

    archive extraction and writing files + black formatting

--- dataset.py
+++ dataset.py
@@ -7,15 +7,56 @@ Created on Tue Nov 05 09:53:17 2019
 
 """
 import os
+import re
+import tarfile
+import time
 
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 from pathlib import Path
-from typing import Dict, List
+from multiprocessing import Pool
+from typing import Dict, List, IO, Union, Optional
+
+import tqdm
+
+
+class_names = ["email", "form", "handwritten", "invoice", "advertisement"]
+class_to_idx = {
+    "letter": 0,
+    "form": 1,
+    "email": 2,
+    "handwritten": 3,
+    "advertisement": 4,
+    "scientific report": 5,
+    "scientific publication": 6,
+    "specification": 7,
+    "file folder": 8,
+    "news article": 9,
+    "budget": 10,
+    "invoice": 11,
+    "presentation": 12,
+    "questionnaire": 13,
+    "resume": 14,
+    "memo": 15,
+}
+
+
+already_done = ("d", "j")
+
+
+def timeit(f):
+    def wrap(*args, **kwargs):
+        t = time.time()
+        f(*args, **kwargs)
+        print(f"Elapsed time: {time.time() - t:0.3f}s")
+
+    return wrap
 
 
 def parse_labels(label_path: Path, data_filter_f=None) -> Dict[int, List[str]]:
     label_d = defaultdict(list)
-    with label_path.open('r') as label_file:
+    with label_path.open("r") as label_file:
         for line in label_file:
             filename, label = line.split()
 
@@ -26,8 +67,10 @@ def parse_labels(label_path: Path, data_filter_f=None) -> Dict[int, List[str]]:
     return label_d
 
 
-def filter_data(filename, label):
-    if filename < 'imagesd/j/' and label in [0, 2, 10, 11, 13, 14]:
+def filter_data(filename: str, label: int):
+    if filename < "images{}/{}/".format(*already_done) and label in [
+        class_to_idx[c] for c in class_names
+    ]:
         return True
     return False
 
@@ -35,18 +78,117 @@ def filter_data(filename, label):
 def get_all_labels(dir_path: Path) -> Dict[int, List[str]]:
     merge_d = defaultdict(list)
     for file in dir_path.iterdir():
-        if file.suffix.endswith('txt'):
+        if file.suffix.endswith("txt"):
             file_d = parse_labels(file, filter_data)
 
             for k, v in file_d.items():
                 merge_d[k].extend(v)
+    for l in merge_d.values():
+        l.sort()
     return merge_d
 
 
-if __name__ == "__main__":
-    label_path = Path(R"F:\labels\\")
-    all_labels = get_all_labels(label_path)
-    
-    print(sum(map(len, all_labels.values())))
-    print({k: len(v) for k, v in all_labels.items()})
+def get_xml_name(tif_name: str) -> str:
+    assert tif_name.endswith("tif")
 
+    new_filename = os.path.split(os.path.split(tif_name)[0])[-1] + ".xml"
+    return os.path.split(tif_name)[0] + "/" + new_filename
+
+
+cpio_re = re.compile(r"images./([a-z])/([a-z]).*")
+
+
+def get_cpio_name(file_name: str) -> Optional[str]:
+
+    match = cpio_re.match(file_name)
+    if match is not None:
+        return "images.{}.{}.cpio".format(*match.groups())
+    else:
+        return None
+    
+    
+def extract_file(file_name: str, dataset_dir: Union[Path, str]) -> Optional[IO]:
+    if isinstance(dataset_dir, str):
+        dataset_dir = Path(dataset_dir)
+
+        assert dataset_dir.is_dir()
+
+    cpio_name = get_cpio_name(file_name)
+
+    if cpio_name is None:
+        return None
+
+    archive = tarfile.open(dataset_dir / Path(cpio_name), "r")
+
+    return archive.extractfile(file_name)
+
+
+def write_file(file: IO, name: str, dataset_dir: Union[Path, str]):
+    assert not file.closed
+
+    if isinstance(dataset_dir, str):
+        dataset_dir = Path(dataset_dir)
+
+        assert dataset_dir.is_dir()
+
+    inter_dir = ""
+    suffix = ".unk"
+    if name.endswith("xml"):
+        inter_dir = "ocr"
+        suffix = ".xml"
+
+    elif name.endswith("tif"):
+        inter_dir = "image"
+        suffix = ".tif"
+
+    inter_dir = Path(inter_dir)
+
+    file_path = (
+        dataset_dir
+        / inter_dir
+        / Path(os.path.split(os.path.split(name)[0])[-1] + suffix)
+    )
+
+    with file_path.open("wb") as out_file:
+        out_file.write(file.read())
+
+
+def extract_and_write(
+    file: str,
+    dataset_dir_src: Union[Path, str],
+    dataset_dir_dst: Union[Path, str]
+):
+    file_stream = extract_file(file, dataset_dir_src)
+    write_file(file_stream, file, dataset_dir_dst)
+
+
+@timeit
+def process_all_files(
+    file_l: List[str],
+    dataset_dir_src: Union[Path, str],
+    dataset_dir_dst: Union[Path, str],
+    max_executors=8,
+):
+    mp_extract_and_write = partial(
+        extract_and_write,
+        dataset_dir_src=dataset_dir_src,
+        dataset_dir_dst=dataset_dir_dst
+    )
+
+    with Pool(max_executors) as executor:
+        executor.map(mp_extract_and_write, file_l)
+        # list(tqdm.tqdm(executor.imap(mp_extract_and_write, file_l), total=len(file_l)))
+
+
+
+if __name__ == "__main__":
+    tobacco_path = Path(r"D:\Tobacco\\")
+    seminaire_path = Path(r"F:\tobacco_dataset\\")
+    label_path = Path(r"F:\labels\\")
+    all_tif_per_label = get_all_labels(label_path)
+
+    all_xml_per_label = {
+        k: list(map(get_xml_name, v)) for k, v in all_tif_per_label.items()
+    }
+
+    process_all_files(all_xml_per_label[11], tobacco_path, seminaire_path)
--- download_dataset.py
+++ download_dataset.py
@@ -11,12 +11,12 @@ import tqdm
 alpha = "abcdefghijklmnopqrstuvwxyz"
 
 
-def download_file(url, local_dir=''):
-    local_filename = url.split('/')[-1]
+def download_file(url, local_dir=""):
+    local_filename = url.split("/")[-1]
     # NOTE the stream=True parameter below
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
-        with open(os.path.join(local_dir, local_filename), 'wb') as f:
+        with open(os.path.join(local_dir, local_filename), "wb") as f:
             for chunk in tqdm.tqdm(r.iter_content(chunk_size=8192)):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
@@ -26,19 +26,23 @@ def download_file(url, local_dir=''):
 
 def download_all_files(max_executors=26, filter_f=None):
     if filter_f is None:
+
         def filter_f(*args, **kwargs):
             return True
 
-    download_file_and_write = partial(download_file, local_dir=R'D:\Tobacco\\')
+    download_file_and_write = partial(download_file, local_dir=r"D:\Tobacco\\")
 
     with ThreadPoolExecutor(max_workers=max_executors) as executor:
-        res = executor.map(download_file_and_write,
-                           map(lambda s: f"https://ir.nist.gov/cdip/cdip-images/images.{'.'.join(s)}.cpio",
-                               filter(filter_f,
-                                      itertools.combinations_with_replacement(alpha, 2))))
+        res = executor.map(
+            download_file_and_write,
+            map(
+                lambda s: f"https://ir.nist.gov/cdip/cdip-images/images.{'.'.join(s)}.cpio",
+                filter(filter_f, itertools.combinations_with_replacement(alpha, 2)),
+            ),
+        )
 
 
 if __name__ == "__main__":
-    already_done = ('d', 'i')
+    already_done = ("d", "i")
 
     download_all_files(26, lambda x: x > already_done)
